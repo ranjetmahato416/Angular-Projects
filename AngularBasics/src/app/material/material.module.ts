e8e82281b64cffb34a5326d8048fb465407ea1c9
ranjetaimhighnepal <ranjetmahato416@gmail.com>
2022-12-08 21:38:59 +0545

    Material Tabs

--- AngularBasics/src/app/material/material.module.ts
+++ AngularBasics/src/app/material/material.module.ts
@@ -10,6 +10,9 @@ import { MatListModule } from '@angular/material/list'
  import { MatDividerModule } from '@angular/material/divider'
  import { MatGridListModule }  from '@angular/material/grid-list'
  import { MatExpansionModule } from '@angular/material/expansion'
+ import { MatCardModule } from '@angular/material/card'
+ import { MatTabsModule } from '@angular/material/tabs'
+ import { MatStepperModule } from '@angular/material/stepper'
 
 const MaterialComponents = [
   MatButtonModule,
@@ -22,7 +25,11 @@ const MaterialComponents = [
   MatListModule,
   MatDividerModule,
   MatGridListModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule
+  
 ];
 
 
